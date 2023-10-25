03330741c0585323f5ad36391bc8795990ba0908
Philip Tran <philiptn@amazon.com>
2023-10-25 14:10:27 +0800

    Updated explanation for setting Lambda constants

--- src/lambda/config/constants.ts
+++ src/lambda/config/constants.ts
@@ -12,15 +12,20 @@ const ENV_URL_THRESHOLD = parseInt(process.env.STATE_MACHINE_URL_THRESHOLD || ''
 export const STATE_MACHINE_URL_THRESHOLD = Number.isInteger(ENV_URL_THRESHOLD) ? ENV_URL_THRESHOLD: 10000;
 
 /**
- * The number of urls to sync in parallel. If increasing this number, consider increasing the read/write capacity for the
- * context table since each parallel sync lambda will read and then possibly write the urls it discovers.
+ * The number of urls to sync in parallel.
  *
- * Note that this number must be less than the STATE_MACHINE_URL_THRESHOLD
- * For Distributed Map state, child execution events are not counted as part of the event history and
- * each batch run requires 5 events to be created (MapStateEntered, MapStateStarted, MapRunStarted, MapRunSucceeded/Failed, MapStateExited)
- * number of batch runs = STATE_MACHINE_URL_THRESHOLD / PARALLEL_URLS_TO_SYNC.
- * Given 25,000 history events and increased concurrency processing, the STATE_MACHINE_URL_THRESHOLD can go much higher.
- * For example, with PARALLEL_URLS_TO_SYNC = 1000, the STATE_MACHINE_URL_THRESHOLD can be set to (25000/5 * 1000 - 5000 * 20)
+ * Distributed Map state can support a maximum concurrency of up to 10,000 child executions in parallel.
+ * However, there is a default concurrency limit of 1000 Lambda executions across all functions in a region, 
+ * we set the concurrency limit for child executions to 1000 by default and the batch size "PARALLEL_URLS_TO_SYNC"
+ * is set to 1000 by default. You can speed up the web crawling further by requesting a quota increase for
+ * the Lambda concurrency limit and update the concurrency limit for the Distributed Map state's child executions
+ * accordingly. 
+ * 
+ * Each batch run requires 5 events to be created (MapStateEntered, MapStateStarted, MapRunStarted, MapRunSucceeded/Failed, 
+ * MapStateExited). Given the hard limit of 25,000 history events for a state machine execution and the child execution events 
+ * for Distributed Map state are not counted as part of the parent event history, the STATE_MACHINE_URL_THRESHOLD can go much 
+ * higher compared to the Inline Map state.
+ * For example, with PARALLEL_URLS_TO_SYNC = 1000, the STATE_MACHINE_URL_THRESHOLD can be set to ~(25000/5 * 1000 - 5000 * 20).
  */
 const ENV_URLS_TO_SYNC = parseInt(process.env.PARALLEL_URLS_TO_SYNC || '')
 export const PARALLEL_URLS_TO_SYNC = Number.isInteger(ENV_URLS_TO_SYNC) ? ENV_URLS_TO_SYNC: 1000;
